56a48a1ccf10197ce4208f65852bcfcff154cddf
MI788212 <ivana.minova@students.finki.ukim.mk>
2026-08-23 21:22:09 +0200

    Add falling off of edge mechanic; Add automatic restart after fall;

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -8,24 +10,84 @@ public class PlayerScript : MonoBehaviour
 {
     public Vector2 InitialPlayerPosition = new Vector2(0, 0);
     public float RollDuration = 0.5f;
+    public float initialLockDuration = 0.5f;
     private float FloorUnit = 1;
     private bool IsRolling { get; set; }
+    private Coroutine rollCoroutine;
     private Orientation PlayerOrientation { get; set; }
+
+    private Rigidbody rb;
+
+    private bool gameOver { get; set; }
+    private bool rollingAllowed { get; set; }
 
     public InputActionReference rollForward;
     public InputActionReference rollBack;
     public InputActionReference rollLeft;
     public InputActionReference rollRight;
 
+    public float gravity = -10f;
+    public float force = 5f;
+
+    private bool wholeBlockFalling = false;
+
     private void Awake()
     {
+        Physics.gravity = new Vector3(0, gravity, 0);
+        rb = GetComponent<Rigidbody>();
         Initialize();
     }
 
     private void Initialize()
     {
-        IsRolling = false;
+        ConstrainRB();
+        transform.position = new Vector3(InitialPlayerPosition.x, 2, InitialPlayerPosition.y);
+        transform.eulerAngles = new Vector3(0, 0, 0);
         PlayerOrientation = Orientation.Y;
+
+        IsRolling = false;
+        gameOver = false;
+        rollingAllowed = false;
+        wholeBlockFalling = false;
+    }
+
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    private void BeginGame()
+    {
+        UnconstrainRB();
+        StartCoroutine(DelayRollingAllowed());
+    }
+
+    private IEnumerator DelayRollingAllowed()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < initialLockDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        ConstrainRB();
+        rollingAllowed = true;
+    }
+
+    private void ConstrainRB()
+    {
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+    private void UnconstrainRB()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.useGravity = true;
+        rb.isKinematic = false;
     }
 
     private void OnEnable()
@@ -43,6 +105,55 @@ public class PlayerScript : MonoBehaviour
         rollRight.action.started -= RollRight;
     }
 
+    public void GameOver(Vector3 emptyTilePosition)
+    {
+        if (gameOver)
+        {
+            wholeBlockFalling = true;
+            return;
+        }
+
+        gameOver = true;
+        rollingAllowed = false;
+
+        StartCoroutine(GameOverCoroutine(emptyTilePosition));
+    }
+
+    public IEnumerator GameOverCoroutine(Vector3 emptyTilePosition)
+    {
+
+        if (rollCoroutine != null)
+        {
+            yield return rollCoroutine;
+        }
+
+        UnconstrainRB();
+        if (!wholeBlockFalling)
+        {
+            rb.AddForceAtPosition(Vector3.down * force, emptyTilePosition + Vector3.up * 0.5f, ForceMode.Impulse);
+        }
+        else
+        {
+            rb.AddForce(Vector3.down * force, ForceMode.Impulse);
+        }
+
+        StartCoroutine(Restart());
+    }
+
+    private IEnumerator Restart()
+    {
+        float restartDelay = 2f;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < restartDelay)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        Initialize();
+        BeginGame();
+    }
     private void RollForward(InputAction.CallbackContext context)
     {
         Roll(Vector3.forward);
@@ -64,8 +175,11 @@ public class PlayerScript : MonoBehaviour
 
     private void Roll(Vector3 rollDirection)
     {
+        if (gameOver||!rollingAllowed)
+            return;
+
         Debug.Log("Roll!");
-        StartCoroutine(RollToDirection(rollDirection));
+        rollCoroutine = StartCoroutine(RollToDirection(rollDirection));
     }
 
     private IEnumerator RollToDirection(Vector3 rollDirection)
@@ -84,9 +198,14 @@ public class PlayerScript : MonoBehaviour
 
             while (elapsedTime < RollDuration)
             {
-                elapsedTime += Time.deltaTime;
+                float previousElapsedTime = elapsedTime;
+                elapsedTime = Mathf.Min(elapsedTime + Time.deltaTime, RollDuration);
 
-                transform.RotateAround(pivotOfRotation, axisOfRotation, (angle * (Time.deltaTime / RollDuration)));
+                float deltaAngle = angle *
+                    ((elapsedTime - previousElapsedTime) / RollDuration);
+
+                transform.RotateAround(pivotOfRotation, axisOfRotation, deltaAngle);
+
                 yield return null;
             }
 
