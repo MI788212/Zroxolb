42a96232b8979bf30a1261aa3ecc787e8e0879fd
MI788212 <ivana.minova@students.finki.ukim.mk>
2026-08-26 21:27:36 +0200

    Remove deleted script components from prefabs; Comment out unfinished switch tile rotation

--- Assets/Scripts/SwitchScript.cs
+++ Assets/Scripts/SwitchScript.cs
@@ -1,4 +1,8 @@
+using Assets.Scripts;
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SwitchScript : MonoBehaviour
 {
@@ -8,18 +12,49 @@ public class SwitchScript : MonoBehaviour
     private void Start()
     {
         toggleTiles = transform.parent.Find("ToggleTiles").transform.gameObject;
+        IsOn = true;
+
         Set(startOn);
+
     }
     public void Toggle()
     {
         IsOn = !IsOn;
+
         toggleTiles.SetActive(IsOn);
+        //foreach(Transform toggleTile in toggleTiles.transform)
+        //{
+        //    StartCoroutine(RotateToggleTile(toggleTile.gameObject, 3));
+        //}
     }
 
+    private IEnumerator RotateToggleTile(GameObject toggleTile, float rotationDuration)
+    {
+        float angle = 180;
+        Vector3 pivotOfRotation = toggleTile.transform.Find("Hinge").transform.position;
+        Vector3 axisOfRotation = toggleTile.transform.Find("Hinge").transform.eulerAngles;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < rotationDuration)
+        {
+            float previousElapsedTime = elapsedTime;
+            elapsedTime = Mathf.Min(elapsedTime + Time.deltaTime, rotationDuration);
+
+            float deltaAngle = angle *
+                ((elapsedTime - previousElapsedTime) / rotationDuration);
+
+            toggleTile.transform.RotateAround(pivotOfRotation, axisOfRotation, deltaAngle);
+
+            yield return null;
+        }
+    }
     private void Set(bool on)
     {
-        toggleTiles.SetActive(on);
-        IsOn = on;
+        if (!on)
+        {
+            Toggle();
+        }
     }
 
 }
